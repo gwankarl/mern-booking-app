f9b27eb327fd52adc4f794d3aa183ba9eecda146
gwankarl <gwankarl@gmail.com>
2025-02-03 19:39:25 +0100

    added my hotel view and page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom';
 import Layout from './layouts/Layout';
 import Register from './pages/Register';
 import SignIn from './pages/SignIn';
 import { useAppContext } from './contexts/AppContext';
 import AddHotel from './pages/AddHotel';
+import MyHotels from './pages/MyHotels';
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -36,15 +42,26 @@ function App() {
         />
 
         {isLoggedIn && (
-          <Route
-            path='/add-hotel'
-            element={
-              <Layout>
-                <AddHotel />
-              </Layout>
-            }
-          />
+          <>
+            <Route
+              path='/add-hotel'
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path='/my-hotels'
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+          </>
         )}
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </Router>
   );
