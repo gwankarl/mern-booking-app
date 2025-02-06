e3d09ec04803970c90e438f84282c064e206ed42
gwankarl <gwankarl@gmail.com>
2025-02-06 12:46:27 +0100

    added my bookings page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditHotel from './pages/EditHotel';
 import Search from './pages/Search';
 import Details from './pages/Details';
 import Booking from './pages/Booking';
+import MyBookings from './pages/MyBookings';
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -92,6 +93,14 @@ function App() {
               element={
                 <Layout>
                   <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path='/my-bookings'
+              element={
+                <Layout>
+                  <MyBookings />
                 </Layout>
               }
             />
